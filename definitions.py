4b57d45b8f0db0657b10d8462b4a2d4a863a78f7
Alexander Hughes <alehugh@nuevaschool.org>
2017-10-31 14:54:16 -0700

    building framework

---
+++ definitions.py
@@ -0,0 +1,124 @@
+import numpy as np 
+
+"""
+Case definition:
+r1 Hits right without hitting circle
+r2 Hits left without hitting circle
+r3 Hits opposite without hitting circle, passing circle on left
+r4 Hits opposite without hitting circle, passing circle on right
+r5 Hits circle, comes back
+r6 Hits circle, goes right
+r7 Hits circle, goes left
+r8 Hits circle, goes opposite
+"""
+
+def r1_minmax_angle(position):
+	return (mn, mx)
+def r2_minmax_angle(position):
+	return (mn, mx)
+def r3_minmax_angle(position):
+	return (mn, mx)
+def r4_minmax_angle(position):
+	return (mn, mx)
+def r5_minmax_angle(position):
+	return (mn, mx)
+def r6_minmax_angle(position):
+	return (mn, mx)
+def r7_minmax_angle(position):
+	return (mn, mx)
+def r8_minmax_angle(position):
+	return (mn, mx)
+
+def r1_update(position, angle):
+	return (newpos, newtheta)
+def r2_update(position, angle):
+	return (newpos, newtheta)
+def r3_update(position, angle):
+	return (newpos, newtheta)
+def r4_update(position, angle):
+	return (newpos, newtheta)
+def r5_update(position, angle):
+	return (newpos, newtheta)
+def r6_update(position, angle):
+	return (newpos, newtheta)
+def r7_update(position, angle):
+	return (newpos, newtheta)
+def r8_update(position, angle):
+	return (newpos, newtheta)
+
+def r1_inverse(position, angle):
+	return (oldpos, oldangle)
+def r2_inverse(position, angle):
+	return (oldpos, oldangle)
+def r3_inverse(position, angle):
+	return (oldpos, oldangle)
+def r4_inverse(position, angle):
+	return (oldpos, oldangle)
+def r5_inverse(position, angle):
+	return (oldpos, oldangle)
+def r6_inverse(position, angle):
+	return (oldpos, oldangle)
+def r7_inverse(position, angle):
+	return (oldpos, oldangle)
+def r8_inverse(position, angle):
+	return (oldpos, oldangle)
+
+def r1_jacobian():
+	pass
+def r2_jacobian():
+	pass
+def r3_jacobian():
+	return 1
+def r4_jacobian():
+	return 1
+def r5_jacobian():
+	pass
+def r6_jacobian():
+	pass
+def r7_jacobian():
+	pass
+def r8_jacobian():
+	pass
+
+# Following dictionaries should be a better format for fast lookup:
+minmaxes = {
+	1: r1_minmax_angle,
+	2: r2_minmax_angle,
+	3: r3_minmax_angle,
+	4: r4_minmax_angle,
+	5: r5_minmax_angle,
+	6: r6_minmax_angle,
+	7: r7_minmax_angle,
+	8: r8_minmax_angle
+}
+updates = {
+	1: r1_update,
+	2: r2_update,
+	3: r3_update,
+	4: r4_update,
+	5: r5_update,
+	6: r6_update,
+	7: r7_update,
+	8: r8_update
+}
+inverses = {
+	1: r1_inverse,
+	2: r2_inverse,
+	3: r3_inverse,
+	4: r4_inverse,
+	5: r5_inverse,
+	6: r6_inverse,
+	7: r7_inverse,
+	8: r8_inverse,
+}
+
+jacobians = {
+	1: r1_jacobian,
+	2: r2_jacobian,
+	3: r3_jacobian,
+	4: r4_jacobian,
+	5: r5_jacobian,
+	6: r6_jacobian,
+	7: r7_jacobian,
+	8: r8_jacobian,
+}
--- sinai.py
+++ sinai.py
@@ -1,1 +1,32 @@
 import numpy as np 
+import time
+from definitions import *
+# This imports all globals and functions from definitions. I jsut did this to save space 
+
+"""
+Case definition:
+r1 Hits right without hitting circle
+r2 Hits left without hitting circle
+r3 Hits opposite without hitting circle, passing circle on left
+r4 Hits opposite without hitting circle, passing circle on right
+r5 Hits circle, comes back
+r6 Hits circle, goes right
+r7 Hits circle, goes left
+r8 Hits circle, goes opposite
+
+See definitons.py to view and edit the function boundaries, updates, etc.
+"""
+
+scale = 1000
+
+case_lookup = np.zeros(shape=(scale, scale), dtype=np.int)
+
+# case_lookup is a lookup table for which case a value is in.
+# 'x' axis is position and 'y' axis is angle
+
+for pos in range(scale):
+	for ang in range(scale):
+		for i in range(1, len(minmaxes) + 1):
+			mnmx = minmaxes[i](pos/float(scale) - (np.pi / 2))
+			if mnmx[0] < ang/float(scale) - 1 < mnmx[1]:
+				case_lookup[pos, ang] = i

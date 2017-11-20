8d9eb55ae397ebd829fdb4e7e7d3edc28750ca64
alpaco <pacpole@nuevaschool.org>
2017-11-19 16:03:33 -0800

    implemented some OOP formatting conventions

--- sinai_matrix_implementation.py
+++ sinai_matrix_implementation.py
@@ -22,7 +22,7 @@ either side?
 See definitons.py to view and edit the function boundaries, updates, etc.
 """
 
-scale = 100
+SCALE = 100
 
 def showfig(to_show, name='colorMap'):
 	fig = plt.figure(figsize=(6, 3))
@@ -40,21 +40,15 @@ def showfig(to_show, name='colorMap'):
 	plt.colorbar(orientation='vertical')
 	plt.show()
 
-def distribution_to_matrix(distribution):
-	matrix = []
-	for i in range(scale):
-		matrix.append([float(i) for i in distribution[scale * i:scale * (i + 1)]])
-	return np.array(matrix)
-
 def loc_from_pos(pos):
-	return 2 * pos/float(scale) - 1
+	return 2 * pos/float(SCALE) - 1
 
 def loc_from_ang(ang):
-	return np.pi * ang/float(scale) - np.pi/2
+	return np.pi * ang/float(SCALE) - np.pi/2
 
 def pos_from_loc(loc):
-	pos = int((scale/2.0)*(loc + 1)) # let's try this for now
-	if 0 <= pos <= scale - 1:
+	pos = int((SCALE/2.0)*(loc + 1)) # let's try this for now
+	if 0 <= pos <= SCALE - 1:
 		return pos
 	return None
 	print "loc during error: ", loc
@@ -62,54 +56,54 @@ def pos_from_loc(loc):
 	raise ValueError("Index out of bounds on call of pos_from_loc")
 
 def ang_from_loc(loc):
-	ang = int((scale/np.pi)*(loc + np.pi/2)) # let's try this for now
-	if 0 <= ang <= scale - 1:
+	ang = int((SCALE/np.pi)*(loc + np.pi/2)) # let's try this for now
+	if 0 <= ang <= SCALE - 1:
 		return ang
 	return None
 	print "loc during error: ", loc
 	print "ang during error: ", ang
 	raise ValueError("Index out of bounds on call of pos_from_loc")
 
-def index_from_case_lookup((pos,ang)):
-	return scale * pos + ang
+def index_from_CASE_LOOKUP((pos,ang)):
+	return SCALE * pos + ang
 
-case_lookup = np.empty(shape=(scale, scale), dtype=np.int)
-case_lookup.fill(-1)
+CASE_LOOKUP = np.empty(shape=(SCALE, SCALE), dtype=np.int)
+CASE_LOOKUP.fill(-1)
 
-# case_lookup is a lookup table for which case a value is in.
+# CASE_LOOKUP is a lookup table for which case a value is in.
 # 'x' axis is position and 'y' axis is angle
 
 if '-l' in sys.argv:
 	print "Loading lookup table from file..."
-	case_lookup = np.load("case_lookup.sn")
+	CASE_LOOKUP = np.load("CASE_LOOKUP.sn")
 else:
 	print "Loading lookup table..."
 	st = time.time()
 	print('|'.rjust(60))
-	for pos in range(scale):
-		for ang in range(scale):
+	for pos in range(SCALE):
+		for ang in range(SCALE):
 			for i in range(len(minmaxes)): #i shifted keys in dictionary to make this nicer. maybe we should shift names of regions too?
 				mnmx = minmaxes[i](loc_from_pos(pos))
 				if mnmx[0] <= loc_from_ang(ang) <= mnmx[1]:
 					#print "nifty"
-					case_lookup[pos, ang] = i
+					CASE_LOOKUP[pos, ang] = i
 					break
-		print('.'.rjust((60 * pos) / scale))
+		print('.'.rjust((60 * pos) / SCALE))
 		sys.stdout.write("\033[F")
-	case_lookup.dump("case_lookup.sn")
+	CASE_LOOKUP.dump("CASE_LOOKUP.sn")
 	print "Done in", time.time() - st, "s."
 
-print "Loading UpMatrix..."
+print "Loading P-F MATRIX..."
 st = time.time()
 print('|'.rjust(60))
-UpMatrix = sparse.eye(scale**2)
-UpMatrix = sparse.lil_matrix(UpMatrix)
-for pos in range(scale):
-	for ang in range(scale):
+PFMATRIX = sparse.eye(SCALE**2)
+PFMATRIX = sparse.lil_matrix(PFMATRIX)
+for pos in range(SCALE):
+	for ang in range(SCALE):
 		# In this situation, casenum is not the case number for the location in statespace we are calculating
 		# rho for. Instead, it is the case numebr to be used for propagating back to the previous location,
 		# namely, same position but negated angle.
-		casenum = case_lookup[pos, -ang]
+		casenum = CASE_LOOKUP[pos, -ang]
 		loc = (loc_from_pos(pos), loc_from_ang(ang)) # this is still correct location. loc[1] to be negated later.
 		if casenum != -1:
 			invrs = updates[casenum](loc[0], - loc[1])
@@ -124,11 +118,11 @@ for pos in range(scale):
 			# This is useful because it saves having to find out the region the thing came from to get an inverse
 			# So yeah this is fine. Also backcase is the case that the inverse is in.
 			if refang != None and refpos != None: #@xander when does that happen?
-				backcase = int(case_lookup[refpos, refang])
-				othercase = int(index_from_case_lookup((pos, ang)))
+				backcase = int(CASE_LOOKUP[refpos, refang])
+				othercase = int(index_from_CASE_LOOKUP((pos, ang)))
 				new = 1 / float(jacobians[backcase](*invrs))
-				UpMatrix[othercase, othercase] = 0
-				UpMatrix[othercase, index_from_case_lookup((refpos, refang))] = new
+				PFMATRIX[othercase, othercase] = 0
+				PFMATRIX[othercase, index_from_CASE_LOOKUP((refpos, refang))] = new
 			else:
 				pass
 				##print "broken"
@@ -137,15 +131,15 @@ for pos in range(scale):
 			#print "broken"
 			# check order of invrs[1] and 0 in the reference to current state. Right now we think current state
 			# should be indexed by currentstate[angle, position]. Should be easy enough to check on.
-		print('.'.rjust((60 * pos) / scale))
+		print('.'.rjust((60 * pos) / SCALE))
 		sys.stdout.write("\033[F")
 
-UpMatrix = sparse.csr_matrix(UpMatrix)
+PFMATRIX = sparse.csr_matrix(PFMATRIX)
 print "Done in", time.time() - st, "s."
 print "Running..."
 
-#print case_lookup
-#print 7 in case_lookup
+#print CASE_LOOKUP
+#print 7 in CASE_LOOKUP
 
 def rho(pos, ang):
 	off = np.sqrt(pos **2 + ang **2)
@@ -160,32 +154,36 @@ class Distribution():
 		else:
 			self.steps_from_start = steps_from_start
 		if not current_state:
-			self.current_state = np.matrix([[rho(loc_from_pos(x), loc_from_ang(y)) for x in range(scale)] for y in range(scale)]).flatten().transpose()
+			self.current_state = np.matrix([[rho(loc_from_pos(x), loc_from_ang(y)) for x in range(SCALE)] for y in range(SCALE)]).flatten().transpose()
 		else:
 			self.current_state = current_state
 		# ^^ start distribution
 
 	def update(self):
-		self.current_state = UpMatrix.dot(self.current_state)
+		self.current_state = PFMATRIX.dot(self.current_state)
 		self.steps_from_start += 1
+
+	def convert_to_matrix(self):
+		matrix = []
+		for i in range(SCALE):
+			matrix.append([float(i) for i in self.current_state[SCALE * i:SCALE * (i + 1)]])
+		return np.array(matrix)
 
 dist = Distribution(rho)
 
 
 # That should do it. probably best to represent this with pyplot
 
-showfig(case_lookup)
+showfig(CASE_LOOKUP)
 """
 for i in range(len(minmaxes)):
-	fig_rules = 5 * (case_lookup == i)
+	fig_rules = 5 * (CASE_LOOKUP == i)
 	showfig(fig_rules, name=str(i)+"boundaries")
 """
 doupdate = 1
-showfig(distribution_to_matrix(dist.current_state))
+showfig(dist.convert_to_matrix())
 
 while doupdate:
 	dist.update()
-	# print dist.current_state.shape
-	# print distribution_to_matrix(dist.current_state)[0][0]
-	showfig(distribution_to_matrix(dist.current_state))
+	showfig(dist.convert_to_matrix())
 	doupdate = input("Update again? 1/0\n")
